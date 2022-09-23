ac544b2365c3f34395e7bc72d35b34b043be4d68
19039 <1903994095@qq.com>
2022-09-23 16:45:03 +0800

    feat: 完成可以自定义style属性的WinText组件

--- lib/src/text/win_text.dart
+++ lib/src/text/win_text.dart
@@ -8,23 +8,27 @@ class WinText extends StatelessWidget {
     Key? key,
     this.selectable = false,
     this.textAlign = TextAlign.start,
+    this.style,
   }) : super(key: key);
 
   final String data;
   final bool selectable;
   final TextAlign textAlign;
+  final TextStyle? style;
 
   @override
   Widget build(BuildContext context) {
+    TextStyle style = this.style?.copyWith(fontFamily: fontFamily) ??
+        TextStyle(fontFamily: fontFamily);
     return selectable
         ? SelectableText(
             data,
-            style: TextStyle(fontFamily: fontFamily),
+            style: style,
             textAlign: textAlign,
           )
         : Text(
             data,
-            style: TextStyle(fontFamily: fontFamily),
+            style: style,
             textAlign: textAlign,
           );
   }
